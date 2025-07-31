dd568e22a4da3a179ff01cc8aa0a494e98529f65
Bill Toner <toner.bill@gmail.com>
2025-07-31 18:00:22 -0400

    add pivotingdata

---
+++ PivotingData.sql
@@ -0,0 +1,38 @@
+/*
+  Pivoting Data
+    three logical phases: grouping on rows, spreading on columns, aggregation
+*/
+
+Select * From dbo.Orders;
+
+Select 
+  empid
+  , custid
+  , Sum(qty) 'sumqty'
+From dbo.Orders
+Group By empid, custid;
+
+-- Pivoting with a grouped query
+
+Select 
+  empid
+  , Sum(Case When custid = 'A' Then qty End) 'A'
+  , Sum(Case When custid = 'B' Then qty End) 'B'
+  , Sum(Case When custid = 'C' Then qty End) 'C'
+  , Sum(Case When custid = 'D' Then qty End) 'D'
+From dbo.Orders
+Group by empid;
+
+-- With Pivot (always work with a table expression)
+
+Select 
+  empid, A, B, C, D
+From (Select empid, custid, qty
+      From dbo.Orders) D
+Pivot(Sum(qty) For custid in (A, B, C, D)) P;
+
+Select 
+  custid, [1], [2], [3]
+From (Select custid, empid, qty
+      From dbo.Orders) D
+Pivot(Sum(qty) For empid in ([1], [2], [3])) P;
